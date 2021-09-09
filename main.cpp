0398371571bcbfd7e30c4e739d42e358d14a33a1
Brian Fopiano <mypsn1550@gmail.com>
2021-09-09 09:57:28 -0700

    basic dump

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 
-int main()
-{
-    std::cout<<"Hello, World!"<<std::endl;
+int main() {
+    int x;
+    std::string sstring;
+    std::cout << "Type a string: ";
+    getline(std::cin, sstring);
+    std::cout << "Type a number: "; // Type a number and press enter
+    std::cin >> x; // Get user input from the keyboard
+
+    for (int i = 0; i < x; i++) {
+        std::cout << sstring;
+        std::cout << "\n";
+    }
     return 0;
 }
