c985c21546955348ddd8b72f73ba7b94f226afc6
Brian Fopiano <mypsn1550@gmail.com>
2021-09-20 11:21:58 -0700

    no clue what the fuck im doing

---
+++ cmake-build-debug/CMakeFiles/BrianFopiano.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "BrianFopiano.exe"
+  "BrianFopiano.exe.manifest"
+  "BrianFopiano.pdb"
+  "CMakeFiles/BrianFopiano.dir/main.cpp.obj"
+  "libBrianFopiano.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BrianFopiano.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,21 +1,68 @@
-#include <iostream>
+/*win_list.c*/
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0600
+#endif
+#include <winsock2.h>
+#include <iphlpapi.h>
+#include <ws2tcpip.h>
+#include <cstdio>
+#include <cstdlib>
 
-using namespace std;
+#pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib, "iphlpapi.lib")
+
+
 
 int main() {
-    int fib[10];
-    fib[0] = 0;
-    fib[1] = 1;
-    int arrSize = sizeof(fib) / sizeof(fib[0]);
-
-
-    cout << fib[0] << ", " << fib[1];
-    for (int i = 2; i < arrSize; i++) {
-        fib[i] = fib[i - 1] + fib[i - 2];
-        cout << ", " << fib[i];
+    WSADATA d;
+    if (WSAStartup(MAKEWORD(2, 2), &d)) {
+        printf("Failed to initialize.\n");
+        return -1;
     }
-    cout << ".";
 
+    DWORD asize = 20000;
+    PIP_ADAPTER_ADDRESSES adapters;
+    do {
+        adapters = (PIP_ADAPTER_ADDRESSES)malloc(asize);
+        if (!adapters) {
+            printf("Couldn't allocate %ld bytes for adapters.\n", asize);
+            WSACleanup();
+            return -1;
+        }
+        int r = GetAdaptersAddresses(AF_UNSPEC, GAA_FLAG_INCLUDE_PREFIX, 0,
+                                     adapters, &asize);
+        if (r == ERROR_BUFFER_OVERFLOW) {
+            printf("GetAdaptersAddresses wants %ld bytes.\n", asize);
+            free(adapters);
+        } else if (r == ERROR_SUCCESS) {
+            break;
+        } else {
+            printf("Error from GetAdaptersAddresses: %d\n", r);
+            free(adapters);
+            WSACleanup();
+            return -1;
+        }
+    } while (!adapters);
+
+    PIP_ADAPTER_ADDRESSES adapter = adapters;
+    while (adapter) {
+        printf("\nAdapter name: %S\n", adapter->FriendlyName);
+        PIP_ADAPTER_UNICAST_ADDRESS address = adapter->FirstUnicastAddress;
+        while (address) {
+            printf("\t%s",
+                   address->Address.lpSockaddr->sa_family == AF_INET ?
+                   "IPv4" : "IPv6");
+            char ap[100];
+            getnameinfo(address->Address.lpSockaddr,
+                        address->Address.iSockaddrLength,
+                        ap, sizeof(ap), 0, 0, NI_NUMERICHOST);
+            printf("\t%s\n", ap);
+            address = address->Next;
+        }
+        adapter = adapter->Next;
+    }
+/*win_list.c continued*/
+    free(adapters);
+    WSACleanup();
     return 0;
 }
-
