df44d68efab1a47e95a6a58c987eae6dc983e7ba
Brian Fopiano <mypsn1550@gmail.com>
2021-09-21 08:17:26 -0700

    still no clue

--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -51,5 +51,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/BrianFopiano.dir/DependInfo.cmake"
+  "CMakeFiles/WinsockExample.dir/DependInfo.cmake"
   )
---
+++ cmake-build-debug/CMakeFiles/WinsockExample.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WinsockExample.dir/main.cpp.obj"
+  "WinsockExample.exe"
+  "WinsockExample.exe.manifest"
+  "WinsockExample.pdb"
+  "libWinsockExample.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WinsockExample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
